e239e04033e2a193ae3c075ff48e9c457351c826
gianmadd <gianmarcoaddati@gmail.com>
2024-11-21 00:59:32 +0100

    update populate_db for teams, players, playerstatistics

--- src/db/populate_database.py
+++ src/db/populate_database.py
@@ -36,7 +36,8 @@ team_ids = {
     "MONZA": "1579",
 }
 
-seasons = ["2021", "2022", "2023"]
+seasons = ["2019", "2020", "2021", "2022", "2023"]
+SEASON = "2023"
 
 
 def load_json_data(field_name, filepath):
@@ -75,6 +76,7 @@ def insert_countries(data, conn):
                 (item["name"], item.get("code"), item.get("flag")),
             )
     conn.commit()
+    print("Tabella 'countries' popolata con successo.")
 
 
 def insert_timezones(data, conn):
@@ -89,6 +91,7 @@ def insert_timezones(data, conn):
                 (tz,),
             )
     conn.commit()
+    print("Tabella 'timezone' popolata con successo.")
 
 
 def insert_leagues(data, conn):
@@ -109,6 +112,7 @@ def insert_leagues(data, conn):
                 ),
             )
     conn.commit()
+    print("Tabella 'leagues' popolata con successo.")
 
 
 def insert_teams(data, conn):
@@ -119,9 +123,9 @@ def insert_teams(data, conn):
         data (list): Lista di dizionari contenenti i dati delle squadre.
         conn: Connessione al database.
     """
+
     with conn.cursor() as cur:
         for item in data:
-            team = item["team"]  # Prende i dati relativi al team
             cur.execute(
                 """
                 INSERT INTO teams (
@@ -131,13 +135,13 @@ def insert_teams(data, conn):
                 ON CONFLICT (id) DO NOTHING;
                 """,
                 (
-                    team["id"],
-                    team["name"],
-                    team.get("code"),
-                    team["country"],
-                    team.get("founded"),
-                    team.get("national", False),
-                    team.get("logo"),
+                    item["team_id"],
+                    item["team_name"],
+                    item["team_code"],
+                    item["team_country"],
+                    item["team_founded"],
+                    item["team_national"],
+                    item["team_logo"],
                 ),
             )
     conn.commit()
@@ -149,13 +153,11 @@ def insert_players(data, conn):
     Inserisce i dati dei giocatori nella tabella 'players'.
 
     Args:
-        data (list): Lista di dizionari contenenti i dati dei giocatori.
+        data (dict): Dizionario contenente i dati dei giocatori.
         conn: Connessione al database.
     """
-
     with conn.cursor() as cur:
-        for item in data:
-            player = item["player"]
+        for item in data:  # Itera sulla lista di giocatori
             cur.execute(
                 """
                 INSERT INTO players (
@@ -166,19 +168,19 @@ def insert_players(data, conn):
                 ON CONFLICT (id) DO NOTHING;
                 """,
                 (
-                    player["id"],
-                    player["name"],
-                    player.get("firstname"),
-                    player.get("lastname"),
-                    player.get("age"),
-                    player["birth"].get("date"),
-                    player["birth"].get("place"),
-                    player["birth"].get("country"),
-                    player.get("nationality"),
-                    player.get("height"),
-                    player.get("weight"),
-                    player.get("injured", False),
-                    player.get("photo"),
+                    item["player_id"],
+                    item["player_name"],
+                    item.get("player_firstname"),
+                    item.get("player_lastname"),
+                    item.get("player_age"),
+                    item.get("player_birth_date"),
+                    item.get("player_birth_place"),
+                    item.get("player_birth_country"),
+                    item.get("player_nationality"),
+                    item.get("player_height"),
+                    item.get("player_weight"),
+                    item.get("player_injured", False),
+                    item.get("player_photo"),
                 ),
             )
     conn.commit()
@@ -190,71 +192,69 @@ def insert_player_statistics(data, conn):
     Inserisce i dati delle statistiche dei giocatori nella tabella 'player_statistics'.
 
     Args:
-        data (list): Lista di dizionari contenenti le statistiche dei giocatori.
+        data (dict): Dizionario contenente i dati delle statistiche dei giocatori.
         conn: Connessione al database.
     """
+
     with conn.cursor() as cur:
-        for item in data:
-            player_id = item["player"]["id"]
-            for stat in item["statistics"]:
-                cur.execute(
-                    """
-                    INSERT INTO player_statistics (
-                        player_id, team_id, season, league_id, position, games_appearances,
-                        games_lineups, games_minutes, rating, captain, substitutes_in,
-                        substitutes_out, substitutes_bench, shots_total, shots_on, goals_total,
-                        goals_assists, passes_total, passes_key, passes_accuracy,
-                        tackles_total, tackles_blocks, tackles_interceptions, duels_total,
-                        duels_won, dribbles_attempts, dribbles_success, fouls_drawn,
-                        fouls_committed, cards_yellow, cards_yellowred, cards_red,
-                        penalties_won, penalties_committed, penalties_scored, penalties_missed, penalties_saved
-                    )
-                    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);
-                    """,
-                    (
-                        player_id,
-                        stat["team"]["id"],
-                        stat["league"]["season"],
-                        stat["league"]["id"],
-                        stat["games"].get("position"),
-                        stat["games"].get("appearences"),
-                        stat["games"].get("lineups"),
-                        stat["games"].get("minutes"),
-                        stat["games"].get("rating"),
-                        stat["games"].get("captain"),
-                        stat["substitutes"].get("in"),
-                        stat["substitutes"].get("out"),
-                        stat["substitutes"].get("bench"),
-                        stat["shots"].get("total"),
-                        stat["shots"].get("on"),
-                        stat["goals"].get("total"),
-                        stat["goals"].get("assists"),
-                        stat["passes"].get("total"),
-                        stat["passes"].get("key"),
-                        stat["passes"].get("accuracy"),
-                        stat["tackles"].get("total"),
-                        stat["tackles"].get("blocks"),
-                        stat["tackles"].get("interceptions"),
-                        stat["duels"].get("total"),
-                        stat["duels"].get("won"),
-                        stat["dribbles"].get("attempts"),
-                        stat["dribbles"].get("success"),
-                        stat["fouls"].get("drawn"),
-                        stat["fouls"].get("committed"),
-                        stat["cards"].get("yellow"),
-                        stat["cards"].get("yellowred"),
-                        stat["cards"].get("red"),
-                        stat["penalty"].get("won"),
-                        stat["penalty"].get("commited"),
-                        stat["penalty"].get("scored"),
-                        stat["penalty"].get("missed"),
-                        stat["penalty"].get("saved"),
-                    ),
+        for item in data:  # Itera sulla lista di giocatori
+            cur.execute(
+                """
+                INSERT INTO player_statistics (
+                    player_id, team_id, season, league_id, position, games_appearances, games_lineups, games_minutes, rating, captain,
+                    substitutes_in, substitutes_out, substitutes_bench, shots_total, shots_on, goals_total,
+                    goals_assists, passes_total, passes_key, passes_accuracy,
+                    tackles_total, tackles_blocks, tackles_interceptions, duels_total, duels_won,
+                    dribbles_attempts, dribbles_success, fouls_drawn, fouls_committed,
+                    cards_yellow, cards_yellowred, cards_red, penalties_won, penalties_committed,
+                    penalties_scored, penalties_missed, penalties_saved
                 )
+                VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s,
+                        %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+                ON CONFLICT DO NOTHING;
+                """,
+                (
+                    item.get("player_id"),
+                    item.get("statistics_team_id"),
+                    item.get("statistics_league_season"),
+                    item.get("statistics_league_id"),
+                    item.get("statistics_games_position"),
+                    item.get("statistics_games_appearences"),
+                    item.get("statistics_games_lineups"),
+                    item.get("statistics_games_minutes"),
+                    item.get("statistics_games_rating"),
+                    item.get("statistics_games_captain"),
+                    item.get("statistics_substitutes_in"),
+                    item.get("statistics_substitutes_out"),
+                    item.get("statistics_substitutes_bench"),
+                    item.get("statistics_shots_total"),
+                    item.get("statistics_shots_on"),
+                    item.get("statistics_goals_total"),
+                    item.get("statistics_goals_assists"),
+                    item.get("statistics_passes_total"),
+                    item.get("statistics_passes_key"),
+                    item.get("statistics_passes_accuracy"),
+                    item.get("statistics_tackles_total"),
+                    item.get("statistics_tackles_blocks"),
+                    item.get("statistics_tackles_interceptions"),
+                    item.get("statistics_duels_total"),
+                    item.get("statistics_duels_won"),
+                    item.get("statistics_dribbles_attempts"),
+                    item.get("statistics_dribbles_success"),
+                    item.get("statistics_fouls_drawn"),
+                    item.get("statistics_fouls_committed"),
+                    item.get("statistics_cards_yellow"),
+                    item.get("statistics_cards_yellowred"),
+                    item.get("statistics_cards_red"),
+                    item.get("statistics_penalty_won"),
+                    item.get("statistics_penalty_committed"),
+                    item.get("statistics_penalty_scored"),
+                    item.get("statistics_penalty_missed"),
+                    item.get("statistics_penalty_saved"),
+                ),
+            )
     conn.commit()
     print("Tabella 'player_statistics' popolata con successo.")
-
-
 
 
 def populate_tables():
@@ -263,25 +263,54 @@ def populate_tables():
         truncate_tables(conn)
 
         insert_countries(
-            load_json_data("countries", os.path.join(f"{DATA_PATH_GENERIC}", "countries.json")), conn
+            load_json_data(
+                "countries", os.path.join(f"{DATA_PATH_GENERIC}", "countries.json")
+            ),
+            conn,
         )
 
         insert_timezones(
-            load_json_data("timezone", os.path.join(f"{DATA_PATH_GENERIC}", "timezone.json")), conn
+            load_json_data(
+                "timezone", os.path.join(f"{DATA_PATH_GENERIC}", "timezone.json")
+            ),
+            conn,
         )
 
         insert_leagues(
-            load_json_data("leagues", os.path.join(f"{DATA_PATH_GENERIC}", "leagues.json")), conn
+            load_json_data(
+                "leagues", os.path.join(f"{DATA_PATH_GENERIC}", "leagues.json")
+            ),
+            conn,
         )
-        
-        # for season in seasons:
 
-        #     insert_teams(
-        #         load_json_data(
-        #             os.path.join(f"{DATA_PATH_SPECIFIC}/teams", f"135_{season}.json")
-        #         ),
-        #         conn,
-        #     )
+        for season in seasons:
+            insert_teams(
+                load_json_data(
+                    "teams",
+                    os.path.join(f"{DATA_PATH_SPECIFIC}/teams", f"135_{season}.json"),
+                ),
+                conn,
+            )
+
+        insert_players(
+            load_json_data(
+                "players",
+                os.path.join(
+                    f"{DATA_PATH_SPECIFIC}/players/{season}", f"487_{SEASON}.json"
+                ),
+            ),
+            conn,
+        )
+
+        insert_player_statistics(
+            load_json_data(
+                "players",
+                os.path.join(
+                    f"{DATA_PATH_SPECIFIC}/players/{season}", f"487_{SEASON}.json"
+                ),
+            ),
+            conn,
+        )
 
         # for season in seasons:
 
@@ -289,14 +318,22 @@ def populate_tables():
 
         #         insert_players(
         #             load_json_data(
-        #                 os.path.join(f"{DATA_PATH_SPECIFIC}/players/{season}", f"{team_id}_{season}.json")
+        #                 "players",
+        #                 os.path.join(
+        #                     f"{DATA_PATH_SPECIFIC}/players/{season}",
+        #                     f"{team_id}_{season}.json",
+        #                 ),
         #             ),
         #             conn,
         #         )
 
         #         insert_player_statistics(
         #             load_json_data(
-        #                 os.path.join(f"{DATA_PATH_SPECIFIC}/players/{season}", f"{team_id}_{season}.json")
+        #                 "players",
+        #                 os.path.join(
+        #                     f"{DATA_PATH_SPECIFIC}/players/{season}",
+        #                     f"{team_id}_{season}.json",
+        #                 ),
         #             ),
         #             conn,
         #         )
